ed7257f325e55dbcb4f991c91c110739e792811b
ACO1598 <alanclecholiveros@gmail.com>
2019-05-03 13:18:15 +0200

    Version2

--- Filosofos_Practica2.py
+++ Filosofos_Practica2.py
@@ -5,14 +5,14 @@ def Filosofo(id, forks, S):
 	comido=0
 	while(comido==0):
 
-		S.acquire()
 		if(forks[id%5]==1 & forks[(id-1)%5]== 1):
+			S.acquire()
 			forks[id%5]=0
 			forks[(id-1)%5]=0
 			print("Filosofo {0} comiendo".format(id+1))
 			S.release()
 			comido=1
-			waiting.release()
+
 			time.sleep(5)
 			
 
@@ -20,13 +20,15 @@ def Filosofo(id, forks, S):
 			forks[id%5]=1
 			forks[(id-1)%5]=1
 			print("Filosofo {0} ha comido".format(id+1))
-			time.sleep(1)
+			#time.sleep(1)
 			S.release()
+			waiting.release()
+			
 		else:
 			S.release()
 			print("Filosofo {0} esperando".format(id+1))
 			waiting.acquire()
-			time.sleep(1)
+			#time.sleep(1)
 		
 
 forks=[1,1,1,1,1]
